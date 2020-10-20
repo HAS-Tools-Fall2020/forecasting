e9b6783c017652e06e475a819d4d5aae4e98d1b2
Lourdes <lourdesfierro@email.arizona.edu>
2020-10-19 18:09:36 -0700

    Bonus points
    Simple approach try after overthinking it :(

--- evaluation_scripts/Bonus_wk8.py
+++ evaluation_scripts/Bonus_wk8.py
@@ -4,27 +4,13 @@ import numpy as np
 from glob import glob
 import os
 import random      
-import eval_functions as ev_fn         
-#%%
-bonus_names = ['Adam', 'Patrick', 'Ty'] #names  to aply  bonus  to
-weeknum = 5 #forecast week -- this should be the forecast_week you are judging
-            # The same as the forecast week number you used in the score_weekly
-random_flag  = False #Set this to true if you wan to override the names
-               # listed  here and asign points randomly 
-             
-           
-ev_fn.write_bonus(bonus_names,weeknum)
+import eval_functions as ev_fn   
+from itertools import chain
+from collections import Counter
 
-# %% This week method
+#%% Functions
 
-weeknum = 7  #forecast week -- this should be the forecast_week you are judging
-             # The same as the forecast week number you used in the score_weekly
-filename = 'forecast_week' + str(weeknum) + '_results.csv'
-filepath = os.path.join('../weekly_results', filename)
-data = pd.read_csv(filepath)
 
-#%% test 
-weeknum = 7
 def data_week(week_no):
     filename_2 = 'forecast_week' + str(week_no) + '_results.csv'
     filepath_2 = os.path.join('../weekly_results', filename_2)
@@ -36,32 +22,49 @@ def data_week(week_no):
     names_yes = data_new_yes["name"]
     return(names_no,names_yes)
 
-# Looking at students that didn;e get a point 
-# in the previous 3 weeks
-testno,testyes = data_week(weeknum)
-print(testno)
-print(testyes)
-test_1n0,test_1yes = data_week(weeknum-1)
-print(test_1n0)
-print(test_1yes)
-test_2no,test_2yes = data_week(weeknum-2)
-print(test_1n0)
-print(test_2yes)
+def past_bonus(week_no):
+    filename_3 = 'bonus_week' + str(week_no) + '.csv'
+    filepath_3 = os.path.join('../weekly_results', filename_3)
+    data_3 = pd.read_csv(filepath_3)
+    bonus_ppl = data_3.loc[(data_3['points'] == 0)] 
+    ppl_names = bonus_ppl["name"]
+    return(ppl_names)
 
-#%% Addign those names to a list
-frames_no = [testno, test_1n0, test_2no]
-frames_yes = [testyes, test_1yes, test_2yes]
-all_names_no = pd.concat(frames_no)
-all_names_yes = pd.concat(frames_yes)
 
-print("Students that didn't get a point last 3 weeks",all_names_no)
-print("Students that got a point last in the previous last 3 weeks",all_names_yes)
+def long_selection(pp,val): 
+    st = pd.DataFrame(pp) # Dataframe
+    st_tr= st.transpose() # Good formatting
+    st_tr.columns = ["Week2", "Week3", "Week4", "Week5", "Week6","Week7"] # Useful head
+    tt = st_tr.apply(pd.Series.value_counts, axis=1) # To count how many times people has get bonus points
+    tt_st = st_tr.count(axis='columns')
+    t = pd.DataFrame(tt_st)
+    t.columns = ["frequency"]
+    ix = tt[t['frequency'] == val].index.tolist() 
+    names = st_tr["Week2"].iloc[ix]
+    return(names)
 
-# Here I delete names that pople that has gotten a point
-# Here I randmly select the 3 names that willl get the point htis week
 
-# %% Summs up error from week 1 and week 2
-#data['sum'] = data['1week_difference'] + data['2week_difference']
-#data.sort_values('sum')
-#print(data)
-#data.nlargest(3, 'sum', keep='all')["name"]
+# %% This week method
+weeknum = 8  #forecast week -- this should be the forecast_week you are judging
+             # The same as the forecast week number you used in the score_weekly
+#filename = 'forecast_week' + str(weeknum) + '_results.csv'
+#filepath = os.path.join('../weekly_results', filename)
+#data = pd.read_csv(filepath)
+
+
+a = []
+pp = []
+# People that has not get any bonus point per week
+for i in range(2,weeknum):
+    a = past_bonus(i)
+    pp.append(a)
+valu=6
+candidates_nb = long_selection(pp,valu)
+cd_fn_nb = pd.DataFrame(candidates_nb)
+# Bonus points assigned to
+selection = random.sample(range(0, 10), 3)
+bonus_names = cd_fn_nb["Week2"].iloc[selection]
+
+ev_fn.write_bonus(bonus_names,weeknum)
+
+# %%
