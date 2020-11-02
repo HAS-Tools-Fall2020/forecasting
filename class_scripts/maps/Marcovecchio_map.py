58d6b12c0c6b9a4eb51816de29bf7ef15ace9927
Alexa <alexamarco@email.arizona.edu>
2020-11-02 12:37:55 -0700

    Quick Correction

--- class_scripts/maps/Marcovecchio_map.py
+++ class_scripts/maps/Marcovecchio_map.py
@@ -19,7 +19,7 @@ import os
 file = os.path.join('../data', 'gagesII_9322_sept30_2011.shp')
 gages = gpd.read_file(file)
 
-# Let look at what this is 
+# Let look at what this is
 type(gages)
 gages.head()
 gages.columns
@@ -27,29 +27,29 @@ gages.shape
 
 # Looking at the geometry now
 gages.geom_type
-#check our CRS - coordinate reference system 
+# check our CRS - coordinate reference system
 gages.crs
-#Check the spatial extent 
+# Check the spatial extent
 gages.total_bounds
-#NOTE to selves - find out how to get these all at once
+# NOTE to selves - find out how to get these all at once
+
 
 # %% 
 
 # Zoom  in and just look at AZ
 gages.columns
 gages.STATE.unique()
-gages_AZ=gages[gages['STATE']=='AZ']
+gages_AZ = gages[gages['STATE'] == 'AZ']
 gages_AZ.shape
 
 
-
-# %% 
+# %%
 # adding more datasets
 # https://www.usgs.gov/core-science-systems/ngp/national-hydrography/access-national-hydrography-products
 # https://viewer.nationalmap.gov/basic/?basemap=b1&category=nhd&title=NHD%20View
 
 # Example reading in a geodataframe
-# Watershed boundaries for the lower colorado 
+# Watershed boundaries for the lower colorado
 file = os.path.join('../data', 'WBD_15_HU2_GDB.gdb')
 fiona.listlayers(file)
 HUC6 = gpd.read_file(file, layer="WBDHU6")
@@ -63,24 +63,22 @@ HUC6.crs
 # Add some points
 # UA:  32.22877495, -110.97688412
 # STream gauge:  34.44833333, -111.7891667
-point_list = np.array([[-110.97688412, 32.22877495],
-                       [-111.7891667, 34.44833333]])
+point_list = np.array([-110.97688412, 32.22877495])
 
-#make these into spatial features
+# make these into spatial features
 point_geom = [Point(xy) for xy in point_list]
 point_geom
 
-#mape a dataframe of these points
+# make a dataframe of these points
 point_df = gpd.GeoDataFrame(point_geom, columns=['geometry'],
                             crs=HUC6.crs)
-
 
 
 # %% 
 # Now trying to put it all together - adding our two points to the stream gages 
 
 # Trouble!! we are in two differnt CRS
-gages_AZ.crs 
+gages_AZ.crs
 point_df.crs
 
 # To fix this we need to re-project
@@ -90,10 +88,10 @@ points_project = point_df.to_crs(gages_AZ.crs)
 # to it AND if that CRS is the correct CRS!
 
 # now putting everythign on the plot:
-# Project the basins 
+# Project the basins
 HUC6_project = HUC6.to_crs(gages_AZ.crs)
 
-# Adding a basemap 
+# Adding a basemap
 
 fig, ax = plt.subplots(figsize=(5, 8))
 HUC6_project.boundary.plot(ax=ax, color=None,
