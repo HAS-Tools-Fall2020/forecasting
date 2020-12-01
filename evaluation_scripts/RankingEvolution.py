a695cc5a95b52c580a4c93493435c4dd7ea8d141
Xenia De Gracia <xeniadegracia@email.arizona.edu>
2020-12-01 05:13:10 -0500

    ranking evolution dataframe

--- evaluation_scripts/RankingEvolution.py
+++ evaluation_scripts/RankingEvolution.py
@@ -77,117 +77,131 @@ for f in range(np.min(bonus_nums), np.max(bonus_nums)+1):
         columns={'points': ('fcst' + str(f) + '_bonus')})
 
 
-
 scoreboard['total'] = scoreboard['bonus'] + scoreboard['regular']
-
 
 scoreboard['rank'] = scoreboard.total.rank(method='dense', ascending=False)
 scoreboard = scoreboard.sort_values(by='total', ascending=False)
 print(scoreboard)
 
+
 # %%
-# Write out the scoreboard
-fname='scoreboard.csv'
+# Xenias early morning
+# I need a BIG help woth for loops D:
+
+Points2 = score_weekly['fcst2_1wk'] + score_weekly['fcst2_2wk'] + score_weekly['fcst2_bonus']
+Points3 = score_weekly['fcst3_1wk'] + score_weekly['fcst3_2wk'] + score_weekly['fcst3_bonus']
+Points4 = score_weekly['fcst4_1wk'] + score_weekly['fcst4_2wk'] + score_weekly['fcst4_bonus']
+Points5 = score_weekly['fcst5_1wk'] + score_weekly['fcst5_2wk'] + score_weekly['fcst5_bonus']
+Points6 = score_weekly['fcst6_1wk'] + score_weekly['fcst6_2wk'] + score_weekly['fcst6_bonus']
+Points7 = score_weekly['fcst7_1wk'] + score_weekly['fcst7_2wk'] + score_weekly['fcst7_bonus']
+Points8 = score_weekly['fcst8_1wk'] + score_weekly['fcst8_2wk'] + score_weekly['fcst8_bonus']
+Points9 = score_weekly['fcst9_1wk'] + score_weekly['fcst9_2wk'] + score_weekly['fcst9_bonus']
+Points10 = score_weekly['fcst10_1wk'] + score_weekly['fcst10_2wk'] + score_weekly['fcst10_bonus']
+Points11 = score_weekly['fcst11_1wk'] + score_weekly['fcst11_2wk'] + score_weekly['fcst11_bonus']
+Points12 = score_weekly['fcst12_1wk'] + score_weekly['fcst12_2wk'] + score_weekly['fcst12_bonus']
+Points13 = score_weekly['fcst13_1wk'] + score_weekly['fcst13_2wk'] + score_weekly['fcst13_bonus']
+Points14 = score_weekly['fcst14_1wk'] + score_weekly['fcst14_2wk'] + score_weekly['fcst14_bonus']
+
+Acum2 = Points2
+Acum3 = Acum2 + Points3
+Acum4 = Acum3 + Points4
+Acum5 = Acum4 + Points5
+Acum6 = Acum5 + Points6
+Acum7 = Acum6 + Points7
+Acum8 = Acum7 + Points8
+Acum9 = Acum8 + Points9
+Acum10 = Acum9 + Points10
+Acum11 = Acum10 + Points11
+Acum12 = Acum11 + Points12
+Acum13 = Acum12 + Points13
+Acum14 = Acum13 + Points14
+
+df2 = pd.DataFrame(Acum2)
+df2.columns = ['acum2']
+df2['rank2'] = df2.rank(ascending=False, method='dense')
+df2
+
+df3 = pd.DataFrame(Acum3)
+df3.columns = ['acum3']
+df3['rank3'] = df3.rank(ascending=False, method='dense')
+df3
+
+df4 = pd.DataFrame(Acum4)
+df4.columns = ['acum4']
+df4['rank4'] = df4.rank(ascending=False, method='dense')
+df4
+
+df5 = pd.DataFrame(Acum5)
+df5.columns = ['acum5']
+df5['rank5'] = df5.rank(ascending=False, method='dense')
+df5
+
+df6 = pd.DataFrame(Acum6)
+df6.columns = ['acum6']
+df6['rank6'] = df6.rank(ascending=False, method='dense')
+df6
+
+df7 = pd.DataFrame(Acum7)
+df7.columns = ['acum7']
+df7['rank7'] = df7.rank(ascending=False, method='dense')
+df7
+
+df8 = pd.DataFrame(Acum8)
+df8.columns = ['acum8']
+df8['rank8'] = df8.rank(ascending=False, method='dense')
+df8
+
+df9 = pd.DataFrame(Acum9)
+df9.columns = ['acum9']
+df9['rank9'] = df9.rank(ascending=False, method='dense')
+df9
+
+df10 = pd.DataFrame(Acum10)
+df10.columns = ['acum10']
+df10['rank10'] = df10.rank(ascending=False, method='dense')
+df10
+
+df11 = pd.DataFrame(Acum11)
+df11.columns = ['acum11']
+df11['rank11'] = df11.rank(ascending=False, method='dense')
+df11
+
+df12 = pd.DataFrame(Acum12)
+df12.columns = ['acum12']
+df12['rank12'] = df12.rank(ascending=False, method='dense')
+df12
+
+df13 = pd.DataFrame(Acum13)
+df13.columns = ['acum13']
+df13['rank13'] = df13.rank(ascending=False, method='dense')
+df13
+
+df14 = pd.DataFrame(Acum14)
+df14.columns = ['acum14']
+df14['rank14'] = df14.rank(ascending=False, method='dense')
+df14
+
+# %%
+# Merge Acumulative points
+
+rank_evol2 = df2
+rank_evol3  = pd.merge(rank_evol2, df3, right_index=True, left_index=True)
+rank_evol4  = pd.merge(rank_evol3, df4, right_index=True, left_index=True)
+rank_evol5  = pd.merge(rank_evol4, df5, right_index=True, left_index=True)
+rank_evol6  = pd.merge(rank_evol5, df6, right_index=True, left_index=True)
+rank_evol7  = pd.merge(rank_evol6, df7, right_index=True, left_index=True)
+rank_evol8  = pd.merge(rank_evol7, df8, right_index=True, left_index=True)
+rank_evol9  = pd.merge(rank_evol8, df9, right_index=True, left_index=True)
+rank_evol10  = pd.merge(rank_evol9, df10, right_index=True, left_index=True)
+rank_evol11  = pd.merge(rank_evol10, df11, right_index=True, left_index=True)
+rank_evol12  = pd.merge(rank_evol11, df12, right_index=True, left_index=True)
+rank_evol13  = pd.merge(rank_evol12, df13, right_index=True, left_index=True)
+rank_evol14  = pd.merge(rank_evol13, df14, right_index=True, left_index=True)
+
+# %%
+# Write out the ranking evolution CSV
+fname = 'ranking_weekly.csv'
 filetemp = os.path.join('../weekly_results', fname)
-scoreboard.to_csv(filetemp, index_label='name')
-
-# Write out the scoreboard
-fname = 'score_details.csv'
-filetemp = os.path.join('../weekly_results', fname)
-score_weekly.to_csv(filetemp, index_label='name')
+rank_evol14.to_csv(filetemp, index_label='name')
 
 # %%
-
-for i in range(2, 17):
-    ranking_weekly_evol = pd.DataFrame(data=np.zeros((len(temp))),
-                            index=temp.index,
-                            columns=[('rank' + str(i))])
-    ranking_weekly_csv = pd.DataFrame(data=np.zeros((len(temp))),
-                            index=temp.index,
-                            columns=[('rank' + str(i))])
-    ranking_weekly_evol[('rank' + str(i))] = temp['1week_points']+ temp['2week_points'] + tempB['points']
-    print(ranking_weekly_evol)
-
-=======
-i = 0
-f = 4
-for i in range(np.min(forecast_nums), np.max(forecast_nums)+1):
-    fname = 'forecast_week' + str(i) +  '_results.csv'
-    filetemp = os.path.join('../weekly_results', fname)
-    print(filetemp)
-    temp=pd.read_csv(filetemp, index_col='name')
-    fnameB = 'bonus_week' + str(i) + '.csv'
-    filetempB = os.path.join('../weekly_results', fnameB)
-    print(filetempB)
-    tempB=pd.read_csv(filetempB, index_col='name')
-
-    ranking_weekly_evol = pd.DataFrame(data=np.zeros((len(temp))),
-                            index=temp.index,
-                            columns=[('rank' + str(i))])
-    ranking_weekly_evol[('TotalPoints_Week' + str(i+ 1))] = temp['1week_points'] + temp['2week_points'] + tempB['points'] + ranking_weekly_evol[('TotalPoints_Week' + str(i))]
-    ranking_weekly_evol[('rank' + str(i))] = ranking_weekly_evol[('TotalPoints_Week' + str(i))].rank(ascending=False, method='dense')
-    ranking_weekly_evol = ranking_weekly_evol.sort_values(by=('TotalPoints_Week' + str(i)), ascending=False)
-    
-    # add the values to the week table:
-    ranking_weekly_evol.append(ranking_weekly_evol[('rank' + str(i))])
-
-    print(ranking_weekly_evol)
-
-
-
-# Write out the ranking_weekly
-fname = 'ranking_weekly2.csv'
-filetemp = os.path.join('../weekly_results', fname)
-ranking_weekly_evol.to_csv(filetemp, index_label='name')
-
-# %%
-# NEW TRY
-fname = 'score_details.csv'
-filetemp = os.path.join('../weekly_results', fname)
-print(filetemp)
-score_details = pd.read_csv(filetemp, index_col='name')
-
-# %%
-week1_list = ['fcst2_1wk', 'fcst3_1wk', 'fcst4_1wk', 'fcst5_1wk',
-            'fcst6_1wk', 'fcst7_1wk', 'fcst8_1wk', 'fcst9_1wk',
-            'fcst10_1wk', 'fcst11_1wk', 'fcst12_1wk',
-            'fcst13_1wk']
-week2_list = ['fcst2_2wk', 'fcst3_2wk', 'fcst4_2wk', 'fcst5_2wk',
-            'fcst6_2wk', 'fcst7_2wk', 'fcst8_2wk', 'fcst9_2wk',
-            'fcst10_2wk', 'fcst11_2wk', 'fcst12_2wk',
-            'fcst13_2wk']
-bonus_list = ['fcst2_bonus', 'fcst3_bonus', 'fcst4_bonus',
-            'fcst5_bonus', 'fcst6_bonus', 'fcst7_bonus',
-            'fcst8_bonus', 'fcst9_bonus', 'fcst10_bonus',
-            'fcst11_bonus', 'fcst12_bonus', 'fcst13_bonus',]
-
-
-oneweeks = score_weekly[week1_list]
-twoweeks = score_weekly[week2_list]
-bonus = score_weekly[bonus_list]
-
-weekly_totals = pd.DataFrame(data=np.zeros((np.max(forecast_nums)+1))),
-                index=oneweeks.index,
-                columns=['RankTotal'])
-
-weekly_totals['total'] = oneweeks + twoweeks + bonus
-
-weekly_totals.cumsum(axis=1)
-
-# %%
-weekly_totals_one_two = oneweeks.add(twoweeks, fill_value=1)
-weekly_totals = weekly_totals_one_two.add(bonus, fill_value=1)
-
-weekly_totals = weekly_totals.sort_values(ascending=True, by='name')
-weekly_totals[sorted(weekly_totals.columns)]
-
-# %%
-#2.  Take weekly points Dataframe and convert to cumulative weekly points
-semester_total = weekly_totals.copy()
-
-for i in range(1, (np.max(forecast_nums)+1)):
-    semester_total[:i] = semester_total[:(i)] + weekly_totals[:i]
-
-# 3. Convert  Cumulative weekly points to ranks
-ranking = semester_total.rank(axis=1, ascending=False)
-
